3b0eb1796e4b37dba7aa896fb87f29c93560f7f9
JF100376 <JF100376>
2025-04-02 10:18:16 -0600

    update project

--- FacturasSimples/Views/LoginView.swift
+++ FacturasSimples/Views/LoginView.swift
@@ -156,16 +156,8 @@ struct LoginSectionView: View {
             Spacer()
             
             //TODO remove this button
-            Button(action: iniciarSesion) {
-                Text("Iniciar Sesión")
-                    .fontWeight(.bold)
-                    .clipShape(Capsule())
-                    .foregroundColor(.white)
-                    .frame( maxWidth: .infinity, alignment: .center)
-                    .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
-                    .overlay(Capsule()
-                        .stroke(Color("Dark-Cyan"), lineWidth: 3).shadow(color: .white, radius: 6))
-            }.padding(.bottom)
+//
+            
         }
     }
     
